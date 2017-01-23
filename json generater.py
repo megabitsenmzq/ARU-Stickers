6e81039062cc3040e0ae415ef98b79b4002a3d97
Megabits <megabitsenmzq@gmail.com>
2017-01-23 21:29:27 +0800

    2.7 Update.

--- json generater.py
+++ json generater.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-for i in range(1,282):
+for i in range(1,307):
     print "    {\r"
     print ("      \"filename\" : \"Sticker %d.sticker\"\r" % i)
     print "    },\r"
